7c99447d11495a587cfd20db0094fdfc0abb7602
wkaczurba <witold.kaczurba@gmail.com>
2017-02-27 07:16:34 +0000

    Exernal and Embedded Tomcat work. Eclipse files solving problem added.

--- SpringBoot1/src/main/java/com/Application.java
+++ SpringBoot1/src/main/java/com/Application.java
@@ -2,15 +2,23 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }	
 	
 }
